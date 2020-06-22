1630930c20ff9c07e431a0226373c97883795380
Kateryna <63663649+KaterynaBozhko@users.noreply.github.com>
2020-06-23 00:19:23 +0300

    update store

--- app/models/root-store/root-store.ts
+++ app/models/root-store/root-store.ts
@@ -1,37 +1,38 @@
-import { Instance, SnapshotOut, types, getRoot } from "mobx-state-tree"
-import {MCardy, MItem} from "../cardy.model"
-import {addItem, updateItem, getItems, deleteItem} from "../../services/firebase/firebase.service"
+import { Instance, SnapshotOut, types } from "mobx-state-tree"
 
+import {CardStoreModel} from "../card-store"
+import {UserStoreModel} from "../user-store"
 /**
  * A RootStore model.
  */
 // prettier-ignore
 export const RootStoreModel = types.model("RootStore").props({
-    cards: types.array(MCardy)
+    cardStore: types.optional(CardStoreModel,{}),
+    userStore: types.optional(UserStoreModel,{})
 })
-.actions(self=>({
-    async addCard(newCard){
-        await addItem(newCard, 'cards');
-    },
-    async updateCard(id, newCard){
-        await updateItem(id, newCard, 'cards');
-    },
-    refreshCards(cards){
-        self.cards = cards
-    },
-    async deleteCard(id){
-        await deleteItem(id, 'cards');
-    },
-    async getCards(){
-        let cards = await getItems('cards');
-        let newCards = [];
-        cards.forEach(card=>{
-            newCards.push(MCardy.create({id:card.id,item: MItem.create(card.data()) }));
-        })
-        this.refreshCards(newCards);        
-    },
+// .actions(self=>({
+//     async addCard(newCard){
+//         await addItem(newCard, 'cards');
+//     },
+//     async updateCard(id, newCard){
+//         await updateItem(id, newCard, 'cards');
+//     },
+//     refreshCards(cards){
+//         self.cards = cards
+//     },
+//     async deleteCard(id){
+//         await deleteItem(id, 'cards');
+//     },
+//     async getCards(){
+//         let cards = await getItems('cards');
+//         let newCards = [];
+//         cards.forEach(card=>{
+//             newCards.push(MCardy.create({id:card.id,item: MItem.create(card.data()) }));
+//         })
+//         this.refreshCards(newCards);        
+//     },
     
-}))
+// }))
 
 /**
  * The RootStore instance.
