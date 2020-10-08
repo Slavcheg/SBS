dce02c2a4f4355f1f0f3c85766e4a98a091b060f
Georgi Slavchev <georgi.v.slavchev@gmail.com>
2020-10-08 14:34:16 +0300

    styles moved so can be used from more suggestions components

--- app/components-custom/edit-dialogs/styles.tsx
+++ app/components-custom/edit-dialogs/styles.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { color, spacing } from "../../../theme";
+import { color, spacing } from "../../theme";
 
 export const styles = StyleSheet.create({
     rowSpace: {
