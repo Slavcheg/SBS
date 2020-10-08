c929ced4d964148d8524e23afcddaeee6c9ec2a5
Georgi Slavchev <georgi.v.slavchev@gmail.com>
2020-10-08 14:32:38 +0300

    referrals and gymhalls MST stores update

--- app/models/sub-stores/gymhall-store.ts
+++ app/models/sub-stores/gymhall-store.ts
@@ -1,41 +1,28 @@
-import { types, getSnapshot, ModelPropertiesDeclaration } from "mobx-state-tree"
-import { addItem, updateItem, deleteItem, firebaseSnapShot } from "../../services/firebase/firebase.service"
-import { values } from "mobx";
-
+import { types, SnapshotIn } from "mobx-state-tree"
+import { firebaseFuncs } from '../../services/firebase/firebase.service'
 
 export class GymHallItem {
     name = ''
 }
 
-export const GymHallItemModel = types.model({name: ''})
+export const GymHall = types.model({
+    name: ''
+})
 
 export const GymHallModel = types.model({
     id: "",
-    item: GymHallItemModel
+    item: GymHall
 })
+
+export interface IGymhall2 extends SnapshotIn<typeof GymHall> {}
 
 export const GymHallStoreModel = types.model("RootStore").props({
     gymhalls: types.array(GymHallModel),
     collection: 'gymhalls'
 })
 .actions(self => ({
-
-    async addGymHall(newItem){
-        await addItem(newItem, self.collection);
-    },
-
-    async updateGymHall(id, newItem){
-        await updateItem(id, newItem, self.collection);
-    },
     refreshItems(items){
         self.gymhalls = items
     },
-
-    async deleteGymHall(id){
-        await deleteItem(id, self.collection);
-    },
-
-    async getGymHalls(){
-        firebaseSnapShot({Type: self.collection, RefreshHandler: this.refreshItems});  
-    },
 }))
+.actions(self => firebaseFuncs<IGymhall2>(self.refreshItems, self.collection))
